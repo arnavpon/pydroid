85311bc5b381e24b4eaa0f80165bde1e8f05a64b
Sam Morton <sammorton44@gmail.com>
2023-01-11 16:29:08 -0500

    cleaned up face tracking

--- example/face_tracking.py
+++ example/face_tracking.py
@@ -1,6 +1,9 @@
 import cv2
+from datetime import datetime
 import dlib
 import numpy as np
+
+NUM_LANDMARKS = 68
 
 # initialize model
 detector = dlib.get_frontal_face_detector()
@@ -9,35 +12,31 @@ predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 # load video locally for testing
 vid = cv2.VideoCapture('PXL_20220724_101216902.mp4')
 
+start = datetime.today()
+
 success, frame = vid.read()
-while True:
-    # Capture the image from the webcam
-    ret, image = cap.read()
-    # Convert the image color to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Detect the face
-    rects = detector(gray, 1)
-    print('we have ', len(rects), 'faces')
-    # Detect landmarks for each face
-    for rect in rects:
-        # Get the landmark points
-        shape = predictor(gray, rect)
-	# Convert it to the NumPy Array
-        shape_np = np.zeros((68, 2), dtype="int")
-        for i in range(0, 68):
+while success:
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray, 1)
+
+    for face in faces:
+        shape = predictor(gray, face)
+        shape_np = np.zeros((68, 2), dtype = int)
+        
+        for i in range(0, NUM_LANDMARKS):
             shape_np[i] = (shape.part(i).x, shape.part(i).y)
         shape = shape_np
 
-        # Display the landmarks
         for i, (x, y) in enumerate(shape):
-	    # Draw the circle to mark the keypoint 
-            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 		
-    # Display the image
-    cv2.imshow('Landmark Detection', image)
+    cv2.imshow('Landmark Detection', frame)
 
-    # Press the escape button to terminate the code
     if cv2.waitKey(10) == 27:
         break
 
-cap.release()
+    success, frame = vid.read()
+
+print(f'Run in {datetime.today() - start}.')
+vid.release()
