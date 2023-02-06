426602b44604a6ff318eb22f35b21009645495d8
Sam Morton <sammorton44@gmail.com>
2023-02-06 15:52:00 -0500

    cleared out unused code

--- example/android/app/src/main/python/GetHR.py
+++ example/android/app/src/main/python/GetHR.py
@@ -165,63 +165,6 @@ def _bandpass_helper(lowcut, highcut, fs, order=5):
 
 # ======= End Filtering =======
 
-
-# def bandpass(arr, hamming_window, freq_range):
-
-#     # Generate a Hamming window of the specified size
-#     window = windows.hann(hamming_window)
-
-#     # use short-time Fourier transform (STFT) on the arr
-#     # with hamming window of given size to get the frequency, time, and STFT results
-#     f, t, Zxx = stft(arr, window = window, nperseg = hamming_window)
-#     # print('sam', Zxx.shape)
-#     # plt.plot(Zxx)
-#     # plt.show()
-
-#     # copy the STFT results
-#     Zxx_filtered = np.copy(Zxx)
-
-#     # iterate over each time step in the STFT results
-#     for i in range(Zxx.shape[1]):
-        
-#         # iterate over each freq in the STFT results
-#         for j in range(Zxx.shape[0]):
-            
-#             # check if the freq is outside the given freq range, 
-#             # and set corresponding Zxx copy index to 0 if so
-#             abs_fj = abs(f[j])
-#             print(freq_range, abs_fj)
-#             if abs_fj < freq_range[0] or abs_fj > freq_range[1]:
-#                 Zxx_filtered[j][i] = 0
-#             else: print('escaped', abs_fj)
-
-#     # use inverse STFT to obtain the filtered signal
-#     _, arr_filtered = istft(Zxx_filtered, window = window, nperseg = hamming_window)
-#     return arr_filtered
-
-
-# def bandpass_filter(arr, low, high, fs=30, order=128):
-#     nyquist = fs / 2
-#     low = low / nyquist
-#     high = high / nyquist
-
-#     b, a = butter(order, [low, high], btype='band')
-#     window = hamming(order + 1)
-#     filtered = filtfilt(b, a, arr * window)
-#     return filtered
-
-# def bpf(data, low, high, fs=30, order=3):
-#     nyquist = fs / 2
-#     low = low / nyquist
-#     high = high / nyquist
-#     b, a = butter(order, [low, high], btype='band')
-#     window = hamming(128)
-#     filtered = np.zeros(len(data))
-#     for i in range(0, len(data) - 128, 64):
-#         segment = data[i:i + 128]
-#         filtered[i:i + 128] = filtfilt(b, a, segment * window)
-#     return filtered
-
 # ======= Peak Detection =======
 
 def get_peaks(arr, fr = 30, thresh = 0.25, min_accepted_hr = 20, max_accepted_hr = 120):
@@ -260,88 +203,10 @@ def get_ibis(peaks, fr = 30):
         ibis.append((peaks[i] - peaks[i - 1]) / fr)
     return ibis
 
-# def csi(signal):
-#     x = np.arange(len(signal))
-#     y = signal
-#     sample_frequency = 256
-#     x_new = np.linspace(0, x[-1], int(x[-1] * sample_frequency))
-#     spline = CubicSpline(x, y)
-#     y_new = spline(x_new)
-#     return y_new
-    
-
-# def cubic_spline_interpolation(signal, sample_frequency=256):
-#     time = np.arange(0, len(signal)/sample_frequency, 1/sample_frequency)
-#     spline = CubicSpline(time, signal)
-#     new_time = np.arange(0, len(signal) / sample_frequency, 1 / sample_frequency)
-#     new_time = np.clip(new_time, time.min(), time.max())
-#     new_signal = spline(new_time)
-#     return new_signal
-
-# def get_ibis_w_bvp_peak_detection(arr):
-    
-#     # init an empty list to store the peak locations
-#     peak_locs = []
-
-#     # iterate over the interpolated signal to detect BVP peaks
-#     len_arr = len(arr)
-#     for i in range(1, len_arr - 1):
-#         if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
-#             peak_locs.append(i)
-
-#     # apply NC-VT filtering to the IBIs
-#     return nc_vt_filter(peak_locs)
-
-# def nc_vt_filter(peak_locs, tolerance = 0.3):
-    
-#     # init an empty list to store the IBIs
-#     ibis = []
-
-#     # apply NC-VT filtering to the IBIs
-#     for i in range(1, len(peak_locs) - 1):
-        
-#         curr_ibi = peak_locs[i + 1] - peak_locs[i]
-#         prev_ibi = peak_locs[i] - peak_locs[i - 1]
-        
-#         if (curr_ibi / prev_ibi) >= 1 - tolerance and (curr_ibi / prev_ibi) <= 1 + tolerance:
-#             ibis.append(curr_ibi)
-
-#     return ibis
-
-
-# def check_power_spectrum(signal, sample_rate):
-#     # Compute the FFT of the signal
-#     fft = np.fft.fft(signal)
-    
-#     # Get the frequencies corresponding to the FFT coefficients
-#     freqs = np.fft.fftfreq(len(signal), 1/sample_rate)
-    
-#     # Compute the power spectrum by taking the absolute value squared of the FFT
-#     power_spectrum = np.abs(fft)**2
-    
-#     # Plot the power spectrum
-#     plt.plot(freqs, power_spectrum)
-#     plt.xlabel('Frequency (Hz)')
-#     plt.ylabel('Power')
-#     plt.show()
-
-# def get_interbeat_intervals(signal):
-#     peaks, _ = find_peaks(signal)
-#     ibis = np.diff(peaks)
-#     return ibis
-
-# def filter_interbeat_intervals(interbeat_intervals):
-#     mean_ibi = sum(interbeat_intervals) / len(interbeat_intervals)
-#     tolerance = mean_ibi * 0.3
-#     filtered_intervals = []
-#     for i in range(len(interbeat_intervals)):
-#         if (mean_ibi - tolerance) <= interbeat_intervals[i] <= (mean_ibi + tolerance):
-#             filtered_intervals.append(interbeat_intervals[i])
-#     return filtered_intervals
-
-
 def get_hr(ibis):
     return 60 / np.mean(ibis)
+
+# ===== End Interbeat intervals (IBI) and HR estimation =====
 
 
 def pipeline(path = DEFAULT_CSV_NAME, 
@@ -403,33 +268,6 @@ def pipeline(path = DEFAULT_CSV_NAME,
     print('Mean IBI:', mibis)
     print('HR:', get_hr(ibis))
 
-    # Step 6: Apply bandpass filter to the component
-    # fcomp = bandpass(fcomp, 0.7, 4)
-    # # fcomp = bpf(fcomp, 0.7, 4)
-    # plt.title('Bandpass Filtered Component')
-    # plt.plot(fcomp)
-    # plt.show()
-
-    # # # Step 7: Interpolate signal w/ cubic spline function
-    # interp = cubic_spline_interpolation(fcomp)
-    # ibis = get_interbeat_intervals(interp)
-
-    # ibis = filter_interbeat_intervals(ibis)
-
-    # # each ibi value represents a number of frames
-    # fr = 30
-    # ibis = [ibi / fr for ibi in ibis]
-    # print(np.mean(ibis))
-
-    # plt.plot(ibis)
-    # plt.show()
-    # # # # Step 8: Compute IBIs
-    # # ibis = get_ibis_w_bvp_peak_detection(icomp)
-    # # # plt.plot(ibis)
-    # # # plt.show()
-
-    # # # # Step 9: Return HR
-    # return get_hr(ibis)
 
 
 if __name__ == '__main__':
