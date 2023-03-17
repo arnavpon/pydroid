a616ba205e20012aa5188785091c02356f8dbbb0
Sam Morton <sammorton44@gmail.com>
2023-03-17 11:02:04 -0700

    new wavelet experiment

--- example/android/app/src/main/python/pipeline-dev/pipeline-v1/pipeline.py
+++ example/android/app/src/main/python/pipeline-dev/pipeline-v1/pipeline.py
@@ -29,7 +29,7 @@ SETTINGS = {
 }
 
     
-def pipeline(sig: str or np.array, settings: dict = SETTINGS, with_wavelet = False, 
+def pipeline(sig: str or np.array, settings: dict = SETTINGS, wavelet = None, 
              with_valleys = False, with_fourier = False, bounds: Tuple[int, int] = (0, -1), plot: bool = False):
 
     if isinstance(sig, str):
@@ -48,6 +48,18 @@ def pipeline(sig: str or np.array, settings: dict = SETTINGS, with_wavelet = Fal
         plt.title('Raw rPPG')
         plt.show()
     
+    signal = normalize_amplitude_to_1(signal)
+    if wavelet is not None:
+        # len_before = len(signal)
+        signal = apply_wavelet(signal, wave = 'db2', level = wavelet)
+        # len_after = len(signal)
+        # wavelet_mult = len_after / len_before
+        if plot:
+            plt.plot(signal)
+            plt.title('Wavelet rPPG')
+            plt.show()
+    # else: wavelet_mult = 1
+    
     fhr = get_hr_from_fourier(signal, settings['fr'])
 
     # === Apply moving average to raw rPPG ===
@@ -57,26 +69,14 @@ def pipeline(sig: str or np.array, settings: dict = SETTINGS, with_wavelet = Fal
         plt.plot(signal)
         plt.title('Smoothed rPPG')
         plt.show()
-    
-    if with_wavelet:
-        len_before = len(signal)
-        signal = apply_wavelet(signal, wave = 'db2', level = 1)
-        len_after = len(signal)
-        wavelet_mult = len_after / len_before
-        if plot:
-            plt.plot(signal)
-            plt.title('Wavelet rPPG')
-            plt.show()
-    else: wavelet_mult = 1
 
-    new_fr = int(settings['fr'] * wavelet_mult)
-    signal = normalize_amplitude_to_1(signal)
+    # new_fr = int(settings['fr'] * wavelet_mult)
     # fhr = get_hr_from_fourier(signal, new_fr)
 
     # === Get peaks from smoothed rPPG ===
     peaks = get_peaks(
         signal,
-        new_fr,
+        settings['fr'],
         settings['freq'][1],
         settings['peak_height'],
         settings['slice_filter_thresh'],
@@ -89,7 +89,7 @@ def pipeline(sig: str or np.array, settings: dict = SETTINGS, with_wavelet = Fal
     if with_valleys:
         valleys = get_peaks(
             -signal,
-            new_fr,
+            settings['fr'],
             settings['freq'][1],
             settings['peak_height'],
             settings['slice_filter_thresh'],
@@ -111,7 +111,7 @@ def pipeline(sig: str or np.array, settings: dict = SETTINGS, with_wavelet = Fal
         plt.show()
 
     # === Get IBI and HR from peaks ===
-    ibis = get_ibis(peaks, new_fr, with_valleys = with_valleys)
+    ibis = get_ibis(peaks, settings['fr'], with_valleys = with_valleys)
     hr = get_hr(ibis)
 
     hrv = get_hrv(ibis)
@@ -174,6 +174,8 @@ if __name__ == '__main__':
     S = 5
     E1=[]
     E2=[]
+    E3=[]
+    E4=[]
     for s in range(1, 8):
         if s in [4, 5]: continue
         print(f'Subject {s}:')
@@ -185,6 +187,8 @@ if __name__ == '__main__':
         interval = 20 * 30
         errs1 = []
         errs2 = []
+        errs3 = []
+        errs4 = []
         for i in range(1000, len(truth.rgb) - 1000, interval):
             # print(f'Starting at {i}:')
             sig_interval = [i, i + interval]
@@ -193,8 +197,10 @@ if __name__ == '__main__':
             rgb = truth.rgb[sig_interval[0]: sig_interval[1], :]
             bvp = normalize_amplitude_to_1(truth.bvp[truth_interval[0]: truth_interval[1]])
             
-            _, est_hr, est_hrv1 = pipeline(rgb, with_wavelet = False, with_valleys = True, plot = False)
-            _, est_hr2, est_hrv2 = pipeline(rgb, with_wavelet = False, with_valleys = False, with_fourier = True, plot = False)
+            _, est_hr, est_hrv1 = pipeline(rgb, wavelet = None, with_valleys = False, plot = False)
+            _, est_hr2, est_hrv2 = pipeline(rgb, wavelet = 1, with_valleys = False, plot = False)
+            _, est_hr3, est_hrv3 = pipeline(rgb, wavelet = 2, with_valleys = False, plot = False)
+            _, est_hr4, est_hrv4 = pipeline(rgb, wavelet = 3, with_valleys = False, plot = False)
             _, truth_hr, truth_hrv = pipeline_raw(bvp, settings = raw_bvp_settings, smoothing_window = 20, plot = False)
             
             # print('Estimated HR w/o fourier:', est_hr)
@@ -203,19 +209,31 @@ if __name__ == '__main__':
             # print()
             errs1.append(est_hr - truth_hr)
             errs2.append(est_hr2 - truth_hr)
+            errs3.append(est_hr3 - truth_hr)
+            errs4.append(est_hr4 - truth_hr)
 
         if len(errs1) > 0:
-            print('Sum of the w/o fourier error:', sum(errs1))
-            print('Sum of the w/ fourier error:', sum(errs2))
+            print('Sum of error w/ Wavelet 0:', sum(errs1))
+            print('Sum of error w/ Wavelet 1:', sum(errs2))
+            print('Sum of error w/ Wavelet 2:', sum(errs3))
+            print('Sum of error w/ Wavelet 3:', sum(errs4))
             errs1 = [abs(e) for e in errs1]
             errs2 = [abs(e) for e in errs2]
-            print(f'Average error w/o fourier: {round(np.mean(errs1))} from {len(errs1)} samples')
-            print(f'Average error w/ fourier: {round(np.mean(errs2))} from {len(errs2)} samples')
+            errs3 = [abs(e) for e in errs3]
+            errs4 = [abs(e) for e in errs4]
+            print(f'Average error w/ Wavlet 0: {round(np.mean(errs1))} from {len(errs1)} samples')
+            print(f'Average error w/ Wavlet 1: {round(np.mean(errs2))} from {len(errs2)} samples')
+            print(f'Average error w/ Wavlet 2: {round(np.mean(errs3))} from {len(errs3)} samples')
+            print(f'Average error w/ Wavlet 3: {round(np.mean(errs4))} from {len(errs4)} samples')
             print()
 
             E1.append(np.mean(errs1))
             E2.append(np.mean(errs2))
+            E3.append(np.mean(errs3))
+            E4.append(np.mean(errs4))
 
     print('MAINS:')
-    print('Average error w/o fourier:', round(np.mean(E1)))
-    print('Average error w/ fourier:', round(np.mean(E2)))
+    print('Average error w/ Wavelet 0:', round(np.mean(E1)))
+    print('Average error w/ Wavelet 1:', round(np.mean(E2)))
+    print('Average error w/ Wavelet 2:', round(np.mean(E3)))
+    print('Average error w/ Wavelet 3:', round(np.mean(E4)))
