0925cf13788bc143c5ba6bc32c46097e9e544763
Sam Morton <sammorton44@gmail.com>
2023-03-29 08:27:58 -0700

    ml checkpoint

--- example/android/app/src/main/python/pipeline-dev/pipeline-v1/peaks.py
+++ example/android/app/src/main/python/pipeline-dev/pipeline-v1/peaks.py
@@ -5,7 +5,7 @@ March 6, 2023
 """
 
 import numpy as np
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_prominences
 
     
 def get_peaks(signal: np.ndarray, fr: int, max_freq: float, peak_height: float,
@@ -55,3 +55,67 @@ def _filter_peaks(signal: np.ndarray, peaks: np.ndarray, fr: int,
             # removed += len(to_remove)
     
     return peaks
+
+
+def get_peaks_v2(signal: np.ndarray, fr: int, max_freq: float, peak_height: float,
+    prominence: float or None = None, with_valleys: bool = False,
+    with_min_dist: bool = True):
+    
+    # skip any None values at the beggining of the signal
+    first_index = None
+    for i in range(len(signal)):
+        if signal[i] is not None:
+            first_index = i
+            break
+    signal = signal[first_index: ]
+
+    def _peak_getter(sig):
+        min_dist = fr // max_freq if with_min_dist else 1
+        
+        # if prominence is None, make it 0
+        if prominence is None:
+            prominence = 0
+        
+        peaks, _ = find_peaks(sig, height = peak_height, prominence = prominence, distance = min_dist)
+        
+        prominences = {p + first_index: prom for p, prom in zip(peaks, peak_prominences(sig, peaks)[0])}
+        return [p + first_index for p in peaks], prominences
+
+    peaks, peak_proms = _peak_getter(signal)
+    if with_valleys:
+        valleys, valley_prominences = _peak_getter(-signal)
+
+        peaks = sorted(list(set(peaks + valleys)))
+        peak_proms = {**peak_prominences, **valley_prominences}
+        return peaks, peak_proms
+
+    return peaks, peak_proms
+
+
+
+# def _filter_peaks_v2(signal: np.ndarray, peaks: np.ndarray, fr: int,
+#     slice_filter_thresh: int, perc1: float, perc2: float):
+#     """
+#     Filter peaks with the intent of trying to peaks that are "definitely" noise.
+#     """
+
+#     # first, remove peaks that aren't sufficienrtly above the perc2 percentile. I intentionally
+#     # am comparing the peaks to the entire array, and not just the set of peaks, because I'm not
+#     # making the assumption that a certain percentage of peaks will inherently be noisy. However,
+#     # I am making the assumption that if the peak is insufficiently clear of a certain baseline of
+#     # the entire signal, then it must be noise.
+#     peaks = np.array([p for p in peaks if signal[p] >= np.percentile(signal, perc2)])   
+    
+#     # Peak Walk: remove peaks that are close together, that aren't sufficiently large relative to the
+#     # entire signal. This percentile (perc1) is more stringent than the one used in the first step.
+#     for i in range(0, len(signal) - fr, fr):
+        
+#         j = i + fr
+#         slce = peaks[(peaks >= i) & (peaks < j)]
+        
+#         if len(slce) > slice_filter_thresh:
+#             to_remove = [i for i in range(len(slce)) if signal[slce[i]] < np.percentile(signal[slce], perc1)]
+#             peaks = peaks[~np.isin(peaks, [slce[i] for i in to_remove])]
+#             # removed += len(to_remove)
+    
+#     return peaks
